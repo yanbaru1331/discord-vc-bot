5b7cf0fbcf67729728f77b5417a0a352850850f1
Aokiti <yuukiyuuki223@gmail.com>
2022-11-21 22:58:06 +0900

    入力部分の追加

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const dotenv = require('dotenv');
+const {Client, GatewayIntentBits, SlashCommandBuilder} = require("discord.js");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
 client.once("ready", async () => {
-    const data = [{
-        name: "ping",
-        description: "Replies with Pong!",
-    }];
-    await client.application.commands.set(data, process.env.SERVER_ID);
+    const data = new SlashCommandBuilder()
+        .setName("move")
+        .setDescription("メンバーを指定してvcチャンネルを移動します。")
+        .addMentionableOption(option => option
+            .setName("移動するメンバー")
+            .setDescription("移動するメンバーの名前（例. @VCチャンネル移動bot）")
+        ).addChannelOption(option => option
+            .setName("移動するチャンネル")
+            .setDescription("移動するチャンネルの名称（例. 一般）")
+        );
+
+    await client.application.commands.set([data], process.env.SERVER_ID);
     console.log("Ready!");
 });
 
@@ -20,7 +27,11 @@ client.on("interactionCreate", async (interaction) => {
     if (!interaction.isCommand()) {
         return;
     }
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong！');
+    if (interaction.commandName === "move") {
+        const memberId = interaction.options.getMentionable("移動するメンバー");
+        const channelId = interaction.options.getChannel("移動するチャンネル");
+        const mention = "<@" + memberId + ">";
+
+        await interaction.reply(mention + "を " + channelId.name + " チャンネルに移動しました");
     }
 });
