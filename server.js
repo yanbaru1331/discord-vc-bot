a83fd603bafc4fc0642889ff7b42eeb74dba7a41
Aokiti <yuukiyuuki223@gmail.com>
2022-11-23 19:40:58 +0900

    複数ユーザーのボイスチャンネルを移動する機能追加

--- server.js
+++ server.js
@@ -8,21 +8,47 @@ const client = new Client({
 });
 
 client.once("ready", async () => {
-    const data = new SlashCommandBuilder()
+    const move = new SlashCommandBuilder()
         .setName("move")
         .setDescription("ユーザーを指定してボイスチャンネルを移動します。")
-        .addUserOption(option => option
-            .setName("ユーザー名")
-            .setDescription("移動するユーザーの名前（例. @VCチャンネル移動bot）")
-            .setRequired(true)
-        ).addChannelOption(option => option
+        .addChannelOption(option => option
             .setName("移動先チャンネル")
             .setDescription("移動先チャンネルの名称（例. 一般）")
             .addChannelTypes(2)
             .setRequired(true)
+        ).addUserOption(option => option
+            .setName("ユーザー名")
+            .setDescription("移動するユーザーの名前（例. @VCチャンネル移動bot）")
+            .setRequired(true)
         );
 
-    await client.application.commands.set([data], process.env.SERVER_ID);
+    const moves = new SlashCommandBuilder()
+        .setName("moves")
+        .setDescription("複数人のユーザーを指定してボイスチャンネルを移動します。")
+        .addChannelOption(option => option
+            .setName("移動先チャンネル")
+            .setDescription("移動先チャンネルの名称（例. 一般）")
+            .addChannelTypes(2)
+            .setRequired(true)
+        ).addUserOption(option => option
+            .setName("1")
+            .setDescription("移動するユーザーの名前1")
+            .setRequired(true)
+        ).addUserOption(option => option
+            .setName("2")
+            .setDescription("移動するユーザーの名前2")
+        ).addUserOption(option => option
+            .setName("3")
+            .setDescription("移動するユーザーの名前3")
+        ).addUserOption(option => option
+            .setName("4")
+            .setDescription("移動するユーザーの名前4")
+        ).addUserOption(option => option
+            .setName("5")
+            .setDescription("移動するユーザーの名前5")
+        );
+
+    await client.application.commands.set([move, moves], process.env.SERVER_ID);
     console.log("Ready!");
 });
 
@@ -33,26 +59,59 @@ client.on("interactionCreate", async (interaction) => {
         return;
     }
     if (interaction.commandName === "move") {
-        const user = Object(interaction.options.getUser("ユーザー名"));
         const channel = Object(interaction.options.getChannel("移動先チャンネル"));
+        const user = Object(interaction.options.getUser("ユーザー名"));
 
         const allMembers = await interaction.guild.members.fetch();
         const member = allMembers.filter(m => m.id === user.id).first();
 
         try {
             await member.voice.setChannel(channel);
-            await interaction.reply([member] + " を " + [channel] + " チャンネルに移動しました" + ", ");
+            await successReply(member, channel);
         } catch (e) {
-            console.error(e);
-            if (e instanceof DiscordAPIError) {
-                if (e.rawError.code === 40032) {
-                    await interaction.reply("エラー (" + e.rawError.code + ")： ユーザーがボイスチャンネル上に存在しません");
-                } else {
-                    await interaction.reply("エラー (" + e.rawError.code + ")： ボイスチャンネルの移動に失敗しました");
-                }
-            } else {
-                await interaction.reply("エラー (-1)： ボイスチャンネルの移動に失敗しました");
+            await errorReply(e)
+        }
+    }
+
+    if (interaction.commandName === "moves") {
+        const channel = Object(interaction.options.getChannel("移動先チャンネル"));
+
+        const getUser = (n) => Object(interaction.options.getUser(n.toString()))
+        const users = {
+            [getUser(1).id]: getUser(1),
+            [getUser(2).id]: getUser(2),
+            [getUser(3).id]: getUser(3),
+            [getUser(4).id]: getUser(4),
+            [getUser(5).id]: getUser(5),
+        };
+
+        const allMembers = await interaction.guild.members.fetch();
+        const members = allMembers.filter(m => m.id in users);
+
+        try {
+            for (const member of members.values()) {
+                await member.voice.setChannel(channel);
             }
+            await successReply(members.toJSON(), channel);
+        } catch (e) {
+            await errorReply(e);
+        }
+    }
+
+    async function successReply(member, channel) {
+        await interaction.reply([member] + " を " + [channel] + " チャンネルに移動しました" + ", ");
+    }
+
+    async function errorReply(e) {
+        console.error(e);
+        if (e instanceof DiscordAPIError) {
+            if (e.rawError.code === 40032) {
+                await interaction.reply("エラー (" + e.rawError.code + ")： ユーザーがボイスチャンネル上に存在しません");
+            } else {
+                await interaction.reply("エラー (" + e.rawError.code + ")： ボイスチャンネルの移動に失敗しました");
+            }
+        } else {
+            await interaction.reply("エラー (-1)： ボイスチャンネルの移動に失敗しました");
         }
     }
 });
