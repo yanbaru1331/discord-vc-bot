b92799234561aeffdff8827b6bbd715de923a87f
Aokiti <yuukiyuuki223@gmail.com>
2022-12-24 23:26:12 +0900

    オプションの順を変更

--- commands/channel.ts
+++ commands/channel.ts
@@ -1,7 +1,7 @@
 import {SlashCommandBuilder, CommandInteraction, CommandInteractionOptionResolver, VoiceBasedChannel} from "discord.js";
 import {SlashCommandChannelOption} from "@discordjs/builders";
 
-import {successChannelReply} from "../utils/reply";
+import {errorChannelReply, successChannelReply} from "../utils/reply";
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -9,21 +9,30 @@ module.exports = {
         .setDescription("ボイスチャンネルを指定して、その中にいるメンバー全員を移動します。")
         .addChannelOption((option: SlashCommandChannelOption) =>
             option
-                .setName("どこから")
-                .setDescription("移動元チャンネルの名称（例. 一般）")
+                .setName("どこに")
+                .setDescription("移動先チャンネルの名称（例. 一般）")
                 .addChannelTypes(2)
                 .setRequired(true)
         ).addChannelOption((option: SlashCommandChannelOption) =>
             option
-                .setName("どこに")
-                .setDescription("移動先チャンネルの名称（例. ロビー）")
+                .setName("どこから")
+                .setDescription("移動元チャンネルの名称（例. ロビー）")
                 .addChannelTypes(2)
-                .setRequired(true)
         ),
     async execute(interaction: CommandInteraction) {
         const options = interaction.options as CommandInteractionOptionResolver;
-        const channelFrom = options.getChannel("どこから") as VoiceBasedChannel;
         const channelTo = options.getChannel("どこに") as VoiceBasedChannel;
+        let channelFrom = options.getChannel("どこから") as VoiceBasedChannel;
+
+        if (channelFrom == null) {
+            const currentChannel = (interaction.member as any).voice.channel;
+            if (currentChannel == null) {
+                await errorChannelReply(interaction)
+                return;
+            } else {
+                channelFrom = currentChannel;
+            }
+        }
 
         const channelMembers = channelFrom.members;
         let peopleCount = 0;
