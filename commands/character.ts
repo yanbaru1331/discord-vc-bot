0680bdccb9261470089e067d73b17cbab46fca00
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-08-14 09:34:10 +0900

    オプション名を変更

--- commands/character.ts
+++ commands/character.ts
@@ -8,22 +8,22 @@ import {shuffle} from "../utils/shuffle";
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("character")
-        .setDescription("スマブラSPのキャラクターをランダムに選びます")
+        .setDescription("スマブラのキャラクターをランダムに選びます")
         .addNumberOption((option: SlashCommandNumberOption) =>
             option
-                .setName("キャラクター数1")
+                .setName("チーム1のキャラクター数")
                 .setDescription("チーム1のキャラクター数")
                 .setRequired(true)
         ).addNumberOption((option: SlashCommandNumberOption) =>
             option
-                .setName("キャラクター数2")
+                .setName("チーム2のキャラクター数2")
                 .setDescription("チーム2のキャラクター数")
                 .setRequired(true)
         ),
     async execute(interaction: CommandInteraction) {
         const options = interaction.options as any;
-        const num1 = options.getNumber("キャラクター数1");
-        const num2 = options.getNumber("キャラクター数2");
+        const num1 = options.getNumber("チーム1のキャラクター数");
+        const num2 = options.getNumber("チーム2のキャラクター数");
 
         const characters = ['マリオ', 'ドンキーコング', 'リンク', 'サムス', 'ダークサムス', 'ヨッシー', 'カービィ', 'フォックス', 'ピカチュウ', 'ルイージ', 'ネス', 'キャプテン・ファルコン', 'プリン', 'ピーチ', 'デイジー', 'クッパ', 'アイスクライマー', 'シーク', 'ゼルダ', 'ドクターマリオ', 'ピチュー', 'ファルコ', 'マルス', 'ルキナ', 'こどもリンク', 'ガノンドロフ', 'ミュウツー', 'ロイ', 'クロム', 'Mr.ゲーム＆ウォッチ', 'メタナイト', 'ピット', 'ブラックピット', 'ゼロスーツサムス', 'ワリオ', 'スネーク', 'アイク', 'ポケモントレーナー', 'ディディーコング', 'リュカ', 'ソニック', 'デデデ', 'ピクミン＆オリマー', 'ルカリオ', 'ロボット', 'トゥーンリンク', 'ウルフ', 'むらびと', 'ロックマン', 'WiiFitトレーナー', 'ロゼッタ＆チコ', 'リトル・マック', 'ゲッコウガ', 'Miiファイター格闘', 'Miiファイター剣術', 'Miiファイター射撃', 'パルテナ', 'パックマン', 'ルフレ', 'シュルク', 'クッパJr.', 'ダックハント', 'リュウ', 'ケン', 'クラウド', 'カムイ', 'ベヨネッタ', 'インクリング', 'リドリー', 'シモン', 'リヒター', 'キングクルール', 'しずえ', 'ガオガエン', 'パックンフラワー', 'ジョーカー', '勇者', 'バンジョー＆カズーイ', 'テリー', 'ベレト', 'ミェンミェン', 'スティーブ', 'セフィロス', 'ホムラ／ヒカリ', 'カズヤ', 'ソラ']
 
